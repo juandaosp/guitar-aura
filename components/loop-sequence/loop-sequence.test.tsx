881452b7676ae009ea21fb8b8ff7707879d5dd55
Juan David Ospina <juandaosp12@gmail>
2026-06-12 01:57:18 -0500

    fix vitest syntax error in lint-staged

--- components/loop-sequence/loop-sequence.test.tsx
+++ components/loop-sequence/loop-sequence.test.tsx
@@ -3,5 +3,6 @@ import { describe, it, expect } from 'vitest'
 describe('smoke test -> test environment', () => {
     it('it should execute a real assertion', () => {
         expect(true).toBe(true)
+        expect('test').toBe('test')
     })
 })
