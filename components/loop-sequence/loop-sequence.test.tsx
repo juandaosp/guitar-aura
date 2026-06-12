ead98ea450d16de36db32fe6779e0bfd427b09e3
Juan David Ospina <juandaosp12@gmail>
2026-06-12 01:52:44 -0500

    Add husky and vitest configuration, update ci-cd accordingly

---
+++ components/loop-sequence/loop-sequence.test.tsx
@@ -0,0 +1,7 @@
+import { describe, it, expect } from 'vitest'
+
+describe('smoke test -> test environment', () => {
+    it('it should execute a real assertion', () => {
+        expect(true).toBe(true)
+    })
+})
